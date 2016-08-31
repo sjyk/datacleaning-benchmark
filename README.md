606582c0cab10540f7369667658f94ee102b90b2
sirrice <ewu@cs.columbia.edu>
2016-08-31 09:38:10 -0400

    some more doc

--- README.md
+++ README.md
@@ -4,9 +4,7 @@ This is a set of python decorators that inject a wide range of noise into your m
 
 See `test/testcleaningbenchmark.py` for example code.  
 
-Documentation coming soon.
-
-# Installation and Usage
+# Installation 
 
 Install
 
@@ -21,4 +19,21 @@ Test it
         python test/testcleaningbenchmark.py
 
 
+# Documentation
+
+Documentation coming soon.
+
+## Usage
+
+        # create noise model 
+        g = EscapeCorruptionModel([0], 0.5)  
+
+        # augment pipeline_operation to inject escape corruption noise to its first argument
+        @inject(g)
+        def pipeline_operation(data, args):
+          return data
+
+## List of Noise Models
+
+TBA
 
--- test/testcleaningbenchmark.py
+++ test/testcleaningbenchmark.py
@@ -16,23 +16,35 @@ from cleaningbenchmark.CorruptionModels.TypoCorruptionModel import TypoCorruptio
 from cleaningbenchmark.ModelEval.EvalUtils import generateDirtyTrain
 
 def test1():
+  """
+  Explicitly call functions to create a noisy version of your training data
+  """
+
   d = sklearn.datasets.load_digits(n_class=2)
   print d['data'], d['target']
 
-  # Create a model that injects zipfian noise
+  #
+  # Create a model that injects zipfian noise, and generate noisy data
+  #
   g = ZipfNoiseModel(numpy.shape(d['data']),0.1,[], z=4)
-
   X_train, X_test, y_train, y_test = generateDirtyTrain(d['data'], d['target'], g,None)
+
+
   clf = svm.SVC()
   clf.fit(X_train, y_train)  
   print clf.score(X_test, y_test) 
 
 
 def test2():
+  """
+  Use decorator notation to add noise to a function.
+  The function must take a numpy-compatible dataset as input, and generate a numpy-compatible dataset as output
+  """
   a = [["United States", "a", "b"], ["UC Berkeley", "a"]]
   g = EscapeCorruptionModel([0],0.5)
 
 
+  # when myETL is called, the data argument will be corrupted using the EscapeCorruptionModel
   @inject(g)
   def myETL(data):
     return data
